c1e0217a4276f7be1348ec30577b8c995f46a04a
offiahchiaz <offiahchiaz@gmail.com>
2023-03-28 19:51:30 +0100

    Completed Sigup UI

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,48 @@
+import { useState } from 'react'
+
+// styles
 import styles from './Signup.module.css'
 
 export default function Signup() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, password);
+  }
+
   return (
-    <div>Signup</div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Signup</h2>
+
+      <label>
+        <span>display name:</span>
+        <input 
+          type="text" 
+          onChange={(e) => setName(e.target.value)} 
+          value={name} 
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
   )
 }
